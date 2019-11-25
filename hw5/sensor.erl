ec7bf4e02e9c34eb8ccb759f97a168c31e0f5aff
Alex Kubecka <akubecka@stevens.edu>
2019-11-25 13:28:51 -0500

    Update sensor.erl

--- hw5/sensor.erl
+++ hw5/sensor.erl
@@ -4,12 +4,9 @@
 
 sensor(WatcherPID, SensorPID) ->
     Measurement = rand:uniform(11).
-
     if 
         Measurement == 11 -> exit("anamolous reading");
         true -> WatcherPID!{SensorPID, Measurement}
-    end.
-
-        Sleep_time = rand:uniform(10000),
-        timer:sleep(Sleep_time)
-    }
+    end,
+    Sleep_time = rand:uniform(10000),
+    timer:sleep(Sleep_time).
